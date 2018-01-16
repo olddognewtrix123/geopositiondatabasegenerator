a7664b65cf347284b92bbaa0239603bf37ce63a8
John Gillespie <jesgillespie@gmail.com>
2018-01-16 19:48:24 +0000

    just updated some notes

--- app/controllers/getKey.js
+++ app/controllers/getKey.js
@@ -6,7 +6,7 @@
     var customerKey = document.getElementById("key").innerHTML;  
     var customerKeyS = customerKey.toString();
     var src="https://maps.googleapis.com/maps/api/js?key=" + customerKeyS;
-    //var src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcP1rzlNRaAyz4e8vCv1XFS8MiGKjzzpc";
+    //
     console.log("the type of source is " + typeof src);
     return src;
    }
